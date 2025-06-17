6f92ad99ffacb83cb06552864d8efb8d83d06b2b
bifangzi <dengchangyu@stu.xupt.edu.cn>
2025-06-18 03:39:16 +0800

    修改了线段函数中部分参数

--- 课程设计/find_line.m
+++ 课程设计/find_line.m
@@ -5,12 +5,13 @@ function [H,theta,rho,x,y,lines] = find_line(image)
 [H, theta, rho] = hough(image);
 
 % 寻找Hough变换的峰值点
-peaks = houghpeaks(H, 2);
+
+peaks = houghpeaks(H, 2,"Threshold",0.3*max(H(:)));
 x = theta(peaks(:, 2));
 y = rho(peaks(:, 1));
 
 
 % 根据峰值点找到对应的线段
-lines = houghlines(image, theta, rho, peaks, 'FillGap', 30, 'MinLength', 20);
+lines = houghlines(image, theta, rho, peaks, 'FillGap', 100, 'MinLength', 20);
 end
 
